cd53d032b0b7844561a8579d615489456fa9f715
kjw1237 <kjw1237@gmail.com>
2021-01-18 20:50:05 +0900

    2021.01.18 김진욱
    activity_shop.xml //샵페이지 추가
    ShopActivity.java //샵페이지 추가

---
+++ app/src/main/java/com/egg8/ui/user/ShopActivity.java
@@ -0,0 +1,15 @@
+package com.egg8.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.egg8.R;
+import android.os.Bundle;
+
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
