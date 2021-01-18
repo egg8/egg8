2609142fdc583cb7c2ec86a5990cd7c58da05385
kjw1237 <kjw1237@gmail.com>
2021-01-18 17:41:46 +0900

    2021.01.18 김진욱
    FirstActivity.java
    activity_first.xml // 첫번째 화면
    
    JoinActivity3.java
    activity_join3.xml// 회원정보 입력화면

---
+++ app/src/main/java/com/egg8/ui/user/FirstActivity.java
@@ -0,0 +1,15 @@
+package com.egg8.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.egg8.R;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
---
+++ app/src/main/java/com/egg8/ui/user/JoinActivity3.java
@@ -0,0 +1,15 @@
+package com.egg8.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.egg8.R;
+
+public class JoinActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join3);
+    }
+}
