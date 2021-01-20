1223921a1a42b4e9d0594cc611ab3b19329f25b4
kjw1237 <kjw1237@gmail.com>
2021-01-20 18:10:28 +0900

    2021.01.18 김진욱
    activity_shop.xml //샵페이지 추가
    ShopActivity.java //샵페이지 추가

---
+++ app/src/main/java/com/egg8/common/function/Popup.java
@@ -0,0 +1,70 @@
+package com.egg8.common.function;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import com.egg8.R;
+
+public class Popup {
+
+    private Context context;
+    private WebView wv;
+    private Button cancel;
+    private CheckBox checkbox;
+    private CheckBox checkbox2;
+    private CheckBox checkbox3;
+    private CheckBox checkbox4;
+
+    public Popup(Context context) {
+        this.context = context;
+    }
+
+
+
+    public void callFunction(int status) {
+        final Dialog dlg = new Dialog(context);
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dlg.setContentView(R.layout.activity_popup);
+        dlg.show();
+
+        wv = (WebView) dlg.findViewById(R.id.webView);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        if(status == 1) {
+            wv.loadUrl("http://222.100.239.140:8888/agree/agree1");
+        } else if(status == 2) {
+            wv.loadUrl("http://222.100.239.140:8888/agree/agree2");
+        } else {
+            wv.loadUrl("http://222.100.239.140:8888/agree/agree3");
+        }
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.setWebViewClient(new WebViewClientClass());
+
+        cancel = (Button) dlg.findViewById(R.id.pop_close);
+        checkbox = (CheckBox) dlg.findViewById(R.id.checkbox);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dlg.dismiss();
+            }
+        });
+    }
+    private class WebViewClientClass extends WebViewClient {//페이지 이동
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.d("check URL",url);
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
+
--- app/src/main/java/com/egg8/ui/user/JoinActivity.java
+++ app/src/main/java/com/egg8/ui/user/JoinActivity.java
@@ -7,14 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.egg8.R;
+import com.egg8.common.function.Popup;
 
 public class JoinActivity extends AppCompatActivity {
 
 
     Button nextStep;
     CheckBox check1, check2, check3, check4; //전체동의,서비스,개인정보,광고
+    TextView btn_view1,btn_view2,btn_view3;
 
     public int TERMS_AGREE_1 = 0; // No Check = 0, Check = 1
     public int TERMS_AGREE_2 = 0; // No Check = 0, Check = 1
@@ -29,13 +32,41 @@ public class JoinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_join);
+
+        btn_view1=findViewById(R.id.btn_view1);
+        btn_view2=findViewById(R.id.btn_view2);
+        btn_view3=findViewById(R.id.btn_view3);
 
         check1 = findViewById(R.id.checkbox);
         check2 = findViewById(R.id.checkbox2);
         check3 = findViewById(R.id.checkbox3);
         check4 = findViewById(R.id.checkbox4);
         nextStep = findViewById(R.id.nextBtn);
+
+        btn_view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Popup customDialog1=new Popup(JoinActivity.this);
+                customDialog1.callFunction(1);
+            }
+        });
+        btn_view2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Popup customDialog2=new Popup(JoinActivity.this);
+                customDialog2.callFunction(2);
+            }
+        });
+        btn_view3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Popup customDialog3=new Popup(JoinActivity.this);
+                customDialog3.callFunction(3);
+            }
+        });
+
+
 
         nextStep.setOnClickListener(new View.OnClickListener() {
             @Override
