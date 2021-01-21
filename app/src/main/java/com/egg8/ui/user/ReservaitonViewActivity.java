78a640ea3431936a4a62bbc9344911af31b05480
kjw1237 <kjw1237@gmail.com>
2021-01-21 18:52:58 +0900

    1.21 김진욱 ui 수정,추가

---
+++ app/src/main/java/com/egg8/ui/user/ReservaitonViewActivity.java
@@ -0,0 +1,16 @@
+package com.egg8.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.egg8.R;
+
+public class ReservaitonViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservaiton_view);
+    }
+}
