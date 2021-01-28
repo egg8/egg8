589e98a9487a77c497a950f05ed2f5c90a626095
jaeilkim <w8230@naver.com>
2021-01-29 01:03:44 +0900

    엔롤3 로직 완성

--- app/src/main/java/com/egg8/ui/supp/ChangeCategory.java
+++ app/src/main/java/com/egg8/ui/supp/ChangeCategory.java
@@ -3,14 +3,18 @@ package com.egg8.ui.supp;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-
+import android.widget.ImageButton;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.egg8.R;
+import com.egg8.common.GlobalApplication;
+
+import java.util.ArrayList;
 
 public class ChangeCategory extends AppCompatActivity
         implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
@@ -19,7 +23,10 @@ public class ChangeCategory extends AppCompatActivity
     private Context mCon;
     private CheckBox[] cBox = new CheckBox[6];
     private Button btn_enroll_in;
+    private ImageButton btn_rollback;
     int checked = 0;
+    String result;
+    StringBuffer sb = new StringBuffer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,40 +36,84 @@ public class ChangeCategory extends AppCompatActivity
         mCon = this;
         findId(mAc);
     }
+
     private void findId(Activity v) {
         for (int i = 0; i < cBox.length; i++){
             cBox[i] = v.findViewById(R.id.ck00+i);
+            cBox[i].setOnCheckedChangeListener(this);
         }
         btn_enroll_in = v.findViewById(R.id.btn_enroll_in);
+        btn_enroll_in.setOnClickListener(this);
+        btn_rollback = v.findViewById(R.id.btn_rollback);
+        btn_rollback.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.btn_rollback :
+                checked = 0;
+                sb.delete(0, sb.length());
+                btn_enroll_in.setEnabled(false);
+                btn_rollback.setVisibility(View.INVISIBLE);
+                enabledCheckBox(true);
+                break;
+            case R.id.btn_enroll_in :
+                setSelectedText();
+                result = sb.substring(0, sb.length()-1);
+                GlobalApplication.showToastMsg(mCon,result);
+                break;
+        }
     }
+    private void enabledCheckBox(Boolean b) {
+        if(b == true) {
+            for (int i = 0; i < cBox.length; i++){
+                checked = 0;
+                cBox[i].setChecked(false);
+            }
+        }
+        for (int i = 0; i < cBox.length; i++){
+            checked = 0;
+            cBox[i].setEnabled(b);
+        }
+    }
+
     private void checkedBtnEnabled(){
         if(checked > 0) {
             btn_enroll_in.setEnabled(true);
-            if(checked <= 3) {
-                for (int i = 0; i < cBox.length; i++){
-                    cBox[i].setEnabled(false);
-                }
-            } else {
-                for (int i = 0; i < cBox.length; i++){
-                    cBox[i].setEnabled(true);
-                }
+            if(checked == 3) {
+                enabledCheckBox(false);
+                btn_rollback.setVisibility(View.VISIBLE);
             }
         } else {
             btn_enroll_in.setEnabled(false);
         }
     }
+
+    private void setSelectedText(){
+        for(int i=0; i<cBox.length; i++) {
+            if(cBox[i].isChecked()){
+                sb.append(cBox[i].getText().toString() + "/");
+            }
+        }
+    }
+
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        checkedBtnEnabled();
         if(buttonView.isChecked()){
-            checked ++;
+            if(checked == 0){
+                checked += 1;
+            } else {
+                checked ++;
+            }
+            checkedBtnEnabled();
         } else {
-            checked --;
+            if (checked > 0) {
+                checked --;
+            }
+            if(checked == 0){
+                btn_enroll_in.setEnabled(false);
+            }
         }
     }
 }
