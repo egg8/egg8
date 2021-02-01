332d4827f59a20cac51ef17871b440d5070e13a5
kjw1237 <kjw1237@gmail.com>
2021-02-01 12:49:58 +0900

    2.1 User Main xml

---
+++ app/src/main/java/com/egg8/ui/user/User_Main.java
@@ -0,0 +1,16 @@
+package com.egg8.ui.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.egg8.R;
+
+public class User_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+    }
+}
